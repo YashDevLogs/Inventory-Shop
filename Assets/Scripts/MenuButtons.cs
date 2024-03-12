2fe501edbf9064bdf9a0ee8bcc81e0b3c6187f93
Yash <91522919+YashDevLogs@users.noreply.github.com>
2024-03-12 18:06:41 +0530

    implemented a single cancle button prefab
    cancel button script and prefab which handles the panel closing logic

--- Assets/Scripts/MenuButtons.cs
+++ Assets/Scripts/MenuButtons.cs
@@ -9,38 +9,9 @@ public class MenuButtons : MonoBehaviour
     [SerializeField] private GameObject ShopButton;
     [SerializeField] private GameObject menuButtons;
 
-    [SerializeField] private Button InventoryCancleButton;
-    [SerializeField] private Button ShopCancleButton;
-    [SerializeField] private Button BuyConfCancleButton;
-    [SerializeField] private Button DescriptionCancleButton;
-    [SerializeField] private Button NotEnoghMoneyCancleButton;
-    [SerializeField] private Button NotEnoghSpaceCancleButton;
-    [SerializeField] private Button InventorypanelCancleButton;
 
-
-    [SerializeField] private GameObject InventoryPanel;
-    [SerializeField] private GameObject BuyConformationPanel;
-    [SerializeField] private GameObject DescriptionPanel;
-    [SerializeField] private GameObject MenuButtonsPanel;
-    [SerializeField] private GameObject boughtPanel;
-    [SerializeField] private GameObject NotEnoghMoneypanel;
-    [SerializeField] private GameObject NotEnoghSpacepanel;
-    [SerializeField] private GameObject inventoryDescriptionPanel;
+    [SerializeField] private Button cancleButton;
  
-
-
-
-    private void Start()
-    {
-        InventoryCancleButton.onClick.AddListener(() => PanelToClose(InventoryPanel));
-        ShopCancleButton.onClick.AddListener(() => PanelToClose(ShopPanel));
-        BuyConfCancleButton.onClick.AddListener(() => PanelToClose(BuyConformationPanel));
-        DescriptionCancleButton.onClick.AddListener(() => PanelToClose(DescriptionPanel));
-        NotEnoghMoneyCancleButton.onClick.AddListener(() => PanelToClose(NotEnoghMoneypanel));
-        NotEnoghSpaceCancleButton.onClick.AddListener(() => PanelToClose(NotEnoghSpacepanel));
-        InventorypanelCancleButton.onClick.AddListener(() => PanelToClose(inventoryDescriptionPanel));
-
-    }
 
     public void OpenShop()
     {
@@ -54,22 +25,5 @@ public class MenuButtons : MonoBehaviour
             menuButtons.SetActive(false);
     }
 
-    public void PanelToClose(GameObject Panel)
-    {
-        if (Panel == ShopPanel )
-        { 
-            Panel.SetActive(false);
-            MenuButtonsPanel.SetActive(true);
 
-        }
-        else if(Panel == inventoryDescriptionPanel)
-        {
-            inventoryDescriptionPanel.SetActive(false);
-            menuButtons.SetActive(true);
-        }
-        else
-            Panel.SetActive(false);
-            
-
-    }
 }
